bd496a04f2d73122cc17f5d348ff71a4705fdd60
bruce <cnhxunoo@gmail.com>
2025-05-31 17:32:24 +0800

    chore: suppress transformers warning

--- tts_clone_inference/main.py
+++ tts_clone_inference/main.py
@@ -14,6 +14,8 @@ import librosa
 
 from xtts import Xtts, XTTSConfig
 
+import transformers
+transformers.logging.set_verbosity_error()
 
 def save_wav(
     *, wav: np.ndarray, path: str, sample_rate: int = None, pipe_out=None, **kwargs
